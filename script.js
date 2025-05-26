24bc3826bed83273af911aa705958d0cb7b2630f
ouss-94 <toureousmane99@yahoo.com>
2025-05-26 17:21:56 +0200

    js update

--- script.js
+++ script.js
@@ -13,7 +13,146 @@ function getData() {
        let journal = data.journal
        console.log(journal);
        
-       let header = 
+       //HEADER (Nom du journal)
+       let header = document.querySelector("header")
+       console.log(header);
+       let h2 = document.createElement("h2");
+       h2.textContent = journal.nomJournal;
+       header.appendChild(h2);
+
+       //Phrase d'accroche
+       let h1 = document.createElement("h1");
+       h1.textContent = journal.phraseAccroche;
+       header.appendChild(h1)
+       
+       //Texte
+
+       let p = document.createElement("p");
+       p.textContent = journal.texteAppelAction;
+       header.appendChild(p);
+
+       // Themes
+      let themesContainer = document.createElement('div')
+      themesContainer.className = "themes"
+      let themes = journal.themes;
+       
+
+      themes.forEach(theme => {
+        
+        let carteTheme = document.createElement('div');
+        let themeName = document.createElement("h3")
+        themeName.textContent = theme.nom   
+        carteTheme.appendChild(themeName)
+
+       let themeDescription = document.createElement("p");
+       themeDescription.textContent = theme.description
+       carteTheme.appendChild(themeDescription)
+
+       themesContainer.appendChild(carteTheme)
+      });
+
+       //////
+       header.appendChild(themesContainer)
+
+
+       //MAIN
+       //ARTICLE LE PLUS RECENT
+
+
+       
+       let articlePrincipal = journal.articlePrincipal;
+       let artilesSection = document.getElementById("articles")
+       
+       let titre = document.createElement("h2");
+       titre.textContent = articlePrincipal.titre;
+       
+       artilesSection.appendChild(titre)
+
+       let image = document.createElement("img");
+       image.src = articlePrincipal.image;
+       image.alt = "mainImage"
+       artilesSection.appendChild(image)
+
+       let date = document.createElement("p")
+       date.textContent = articlePrincipal.date
+       artilesSection.appendChild(date)
+
+       let theme = document.createElement("p")
+       theme.textContent = articlePrincipal.theme
+       artilesSection.appendChild(theme)
+
+       let description = document.createElement("p")
+       description.textContent = articlePrincipal.description
+       artilesSection.appendChild(description)
+
+       let articles = data.journal.articles
+       
+       articles.forEach(article => {
+       let artilesSection = document.getElementById("articles")
+       
+       let titre = document.createElement("h2");
+       titre.textContent = article.titre;
+       
+       artilesSection.appendChild(titre)
+
+       let image = document.createElement("img");
+       image.src = article.image;
+       image.alt = "mainImage"
+       artilesSection.appendChild(image)
+
+       let date = document.createElement("p")
+       date.textContent = article.date
+       artilesSection.appendChild(date)
+
+       let theme = document.createElement("p")
+       theme.textContent = article.theme
+       artilesSection.appendChild(theme)
+
+       let description = document.createElement("p")
+       description.textContent = article.description
+       artilesSection.appendChild(description)
+        
+       });
+
+       //section 2
+
+       let auteurs = data.journal.auteurs
+
+       auteurs.forEach(auteur => {
+        
+        let auteursSection = document.getElementById("auteurs")
+       let section = document.createElement("section")
+
+       let auteurs = journal.auteur
+       
+       let prenom = document.createElement("h3")
+       prenom.textContent = auteur.prenom
+       section.appendChild(prenom)
+       
+       let typeExperience = document.createElement("p")
+       typeExperience.textContent = auteur.typeExperience
+       section.appendChild(typeExperience)
+       
+       let presentation = document.createElement("p")
+       presentation.textContent = auteur.presentation
+       section.appendChild(presentation)
+       console.log(section);
+       auteursSection.appendChild(section) 
+
+        
+       });
+       
+       
+       
+       let main = document.getElementById("main")
+
+       let mainImage = document.createElement(`img`)
+       mainImage.src = "images/image.png"
+       mainImage.alt = "image dino"
+       main.appendChild(mainImage)
+       
+       
+       
       
 
       
